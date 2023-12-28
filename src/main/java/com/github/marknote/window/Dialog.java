e904ef51e6c5232e5e3b1a2eda533fce6bc473a5
luoyp <1783508363@qq.com>
2023-12-28 16:16:27 +0800

    添加常用图标

--- src/main/java/com/github/marknote/window/Dialog.java
+++ src/main/java/com/github/marknote/window/Dialog.java
@@ -1,6 +1,7 @@
 package com.github.marknote.window;
 
 import com.intellij.openapi.ui.DialogWrapper;
+import com.intellij.openapi.util.NlsContexts;
 import org.jetbrains.annotations.Nullable;
 
 import javax.swing.*;
@@ -12,7 +13,14 @@ public class Dialog extends DialogWrapper {
     }
 
     @Override
+    public void setTitle(@NlsContexts.DialogTitle String title) {
+        super.setTitle(title);
+    }
+
+    @Override
     protected @Nullable JComponent createCenterPanel() {
+        JLabel title = new JLabel();
+        JTextField titleText = new JTextField();
         return null;
     }
 
